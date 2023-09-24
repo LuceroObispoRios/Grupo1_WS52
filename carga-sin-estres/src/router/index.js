c5791e7c507ac9df6319aa9ab6b8b3b9eceee138
Lucero Obispo Rios <102556567+LuceroObispoRios@users.noreply.github.com>
2023-09-24 10:43:51 -0500

    Update index.js

--- carga-sin-estres/src/router/index.js
+++ carga-sin-estres/src/router/index.js
@@ -6,8 +6,10 @@ import ClientRegistrationForm from '../user-management/pages/ClientRegistrationF
 import CompanyRegistrationForm from '../user-management/pages/CompanyRegistrationForm.vue'
 import CompanySettingsForm from '../user-management/pages/CompanySettingsForm.vue'
 import ClientSettingsForm from '../user-management/pages/ClientSettingsForm.vue'
-import HomeView from "@/public/pages/HomeView.component.vue"
-import bookingList from "@/bookingHistory/pages/booking-list.component.vue"
+import HomeView from "@/public/pages/HomeView.component.vue";
+import FormularioPage from "@/membership/pages/FormularioPage.vue";
+import BoletaModal from "@/membership/pages/BoletaModal.vue";
+import bookingList from "@/bookingHistory/pages/booking-list.component.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,9 +21,21 @@ const router = createRouter({
         { path: '/login', name: 'login', component: LoginForm},
         { path: '/register-client', name: 'register-client', component: ClientRegistrationForm},
         { path: '/register-company', name: 'register-company', component: CompanyRegistrationForm},
-        { path: '/company-settings/:id', name: 'company-settings', component: CompanySettingsForm, props: true},
+        { path: '/company-settings/:id', name: 'company-settings', component: CompanySettingsForm, props:true},
         { path: '/client-settings/:id', name: 'client-settings', component: ClientSettingsForm, props: true},
-        { path: '/bookingHistory', name: 'bookingHistory', component: bookingList}
+        { path: '/bookingHistory', name: 'bookingHistory', component: bookingList},
+        { path: '/formulario', component: FormularioPage },
+        { path: '/boleta', component: BoletaModal,
+            props: (route) => ({
+                nombre: route.query.nombre,
+                apellido: route.query.apellido,
+                ruc: route.query.ruc,
+                direccion: route.query.direccion,
+                tipoMembresia: route.query.tipoMembresia,
+                tipoTarjeta: route.query.tipoTarjeta,
+                numeroBoleta: route.query.numeroBoleta,
+            })
+        }
     ]
 })
 export default router
