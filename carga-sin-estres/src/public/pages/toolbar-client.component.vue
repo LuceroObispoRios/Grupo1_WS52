7fe719f5a061afd1871919e9b6892cdc01adc9e9
Damarists <111830490+Damarists@users.noreply.github.com>
2023-09-24 22:36:40 -0500

    Update toolbar-client.component.vue

--- carga-sin-estres/src/public/pages/toolbar-client.component.vue
+++ carga-sin-estres/src/public/pages/toolbar-client.component.vue
@@ -20,40 +20,23 @@ export default {
   <div>
     <pv-toast></pv-toast>
     <header>
-      <pv-toolbar class="bg-primary">
+      <pv-toolbar class="navbar navbar-expand-md navbar-light fixed-top">
         <template #start>
-          <img src="https://github.com/LuceroObispoRios/Grupo1_WS52/blob/main/Proyecto/image/Cargalogo.png?raw=true" alt="Imagen" style="height: 70px">
+          <img src="https://github.com/LuceroObispoRios/Grupo1_WS52/blob/main/Proyecto/image/Cargalogo.png?raw=true" alt="Imagen" style="height: 40px">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
         </template>
         <template #end>
-          <div class="flex-column">
-            <router-link
-                v-for="item in items"
-                :to="item.to"
-                custom
-                v-slot="{ navigate, href }"
-                :key="item.label"
-            >
-              <template v-if="item.label === 'Cerrar Sesion'">
-                <pv-button
-                    class="logout-button"
-                    :href="href"
-                    @click="navigate"
-                >
-                  {{ item.label }}
-                </pv-button>
-              </template>
-              <template v-else>
-                <pv-button
-                    class="p-button-text text-white"
-                    :href="href"
-                    @click="navigate"
-                >
-                  {{ item.label }}
-                </pv-button>
-              </template>
-            </router-link>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><router-link to="/client-settings/:id" class="nav-link">Editar Perfil</router-link></li>
+              <li class="nav-item"><router-link to="/company-search" class="nav-link">Búsqueda de Empresas</router-link></li>
+              <li class="nav-item"><router-link to="/bookingHistory" class="nav-link">Historial de reserva</router-link></li>
+              <li class="nav-item"><router-link to="/home" class="btn btn-outline-dark">Cerrar Sesión</router-link></li>
+              </ul>
           </div>
-        </template>
+              </template>
       </pv-toolbar>
     </header>
 
@@ -63,8 +46,9 @@ export default {
 </template>
 
 <style scoped>
-.bg-primary {
-  background-color: #282828 !important;
+/* Estilos para la barra de navegación */
+.navbar {
+  background-color: #1f1e1e;
   position: fixed;
   top: 0;
   left: 0;
@@ -72,15 +56,34 @@ export default {
   z-index: 1000;
 }
 
-.logout-button {
-  background-color: #000000; /* Fondo blanco */
-  border-color: white;
-  color: #ffffff; /* Texto de color oscuro */
+.navbar-toggler {
+  background-color: #ffffff !important;
+  margin-left:300px;
 }
 
-.logout-button:hover {
-  background-color: #000000; /* Fondo blanco */
-  border-color: white;
-  color: #ffffff; /* Texto de color oscuro */
+.navbar-toggler-icon {
+  color: #ffffff !important;
+}
+
+.navbar-nav .nav-item {
+  margin-right: 20px;
+}
+
+.nav-link {
+  color: #ffffff !important;
+}
+
+.btn-outline-dark {
+  color: #ffffff;
+  width:150px;
+  background-color: #000000;
+  border-color: #000000;
+  margin-left: 20px;
+}
+
+.btn-outline-dark:hover {
+  color: #ffffff;
+  background-color: #ff8800;
+  border-color: #ffffff;
 }
 </style>
