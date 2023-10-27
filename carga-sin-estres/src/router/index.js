ef14adee667bc479dbbb1cb948bfa2cda029a9d2
Damarists <111830490+Damarists@users.noreply.github.com>
2023-10-26 21:30:48 -0500

    Update index.js

--- carga-sin-estres/src/router/index.js
+++ carga-sin-estres/src/router/index.js
@@ -1,23 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CompanyList from '../company-search/pages/company-list.component.vue'
-import CompanyDetail from '../company-search/pages/detail-company.component.vue'
-import LoginForm from '../user-management/pages/LoginForm.vue'
-import ClientRegistrationForm from '../user-management/pages/ClientRegistrationForm.vue'
-import CompanyRegistrationForm from '../user-management/pages/CompanyRegistrationForm.vue'
-import CompanySettingsForm from '../user-management/pages/CompanySettingsForm.vue'
-import ClientSettingsForm from '../user-management/pages/ClientSettingsForm.vue'
-import HomeView from "../public/pages/HomeView.component.vue";
-import FormularioPage from "../membership/pages/FormularioPage.vue";
-import BoletaModal from "../membership/pages/BoletaModal.vue";
-import bookingList from "../bookingHistory/pages/booking-list.component.vue";
-import ToolbarClientComponent from "@/public/pages/toolbar-client.component.vue";
-import ToolbarCompanyComponent from "@/public/pages/toolbar-company.component.vue";
+import CompanyList from '../companySearch/components/companyList.component.vue'
+import CompanyDetail from '../companySearch/components/detailCompany.component.vue'
+import LoginForm from '../userManagement/components/loginForm.component.vue'
+import ClientRegistrationForm from '../userManagement/components/clientRegistrationForm.component.vue'
+import CompanyRegistrationForm from '../userManagement/components/companyRegistrationForm.component.vue'
+import CompanySettingsForm from '../userManagement/components/companySettingsForm.component.vue'
+import ClientSettingsForm from '../userManagement/components/clientSettingsForm.component.vue'
+import Home from "../public/components/home.component.vue";
+import FormularyCompany from "../membership/components/formularyCompany.component.vue";
+import BoletteCompany from "../membership/components/boletteCompany.component.vue";
+import BookingList from "../bookingHistory/components/bookingList.component.vue";
+import ToolbarClientComponent from "@/public/components/toolbarClient.component.vue";
+import ToolbarCompanyComponent from "@/public/components/toolbarCompany.component.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', redirect: 'home'},
-        { path: '/home', name: 'home', component: HomeView},
+        { path: '/home', name: 'home', component: Home},
         { path: '/login', name: 'login', component: LoginForm},
         { path: '/register-client', name: 'register-client', component: ClientRegistrationForm},
         { path: '/register-company', name: 'register-company', component: CompanyRegistrationForm},
@@ -27,7 +27,7 @@ const router = createRouter({
             children: [
                 { path: 'client-settings', name: 'client-settings', component: ClientSettingsForm},
                 { path: 'company-search', name: 'company-search', component: CompanyList},
-                { path: 'client-booking-history', name: 'client-booking-history', component: bookingList},
+                { path: 'client-booking-history', name: 'client-booking-history', component: BookingList},
                 { path: 'company', name: 'company-detail', component: CompanyDetail, props: true},
             ]
         },
@@ -37,7 +37,7 @@ const router = createRouter({
             path: '/company/:id', component: ToolbarCompanyComponent,
             children: [
                 { path: 'company-settings', name: 'company-settings', component: CompanySettingsForm},
-                { path: 'company-booking-history', name: 'company-booking-history', component: bookingList},
+                { path: 'company-booking-history', name: 'company-booking-history', component: BookingList},
                 { path: 'formulary', name: 'formulary-company', component: FormularyCompany },
                 { path: 'bolette', name: 'bolette-company',
                     component: BoletteCompany,
