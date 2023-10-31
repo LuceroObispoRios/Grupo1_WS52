c07b0e97ca192807ac85232101e05bf87c12750a
Damarists <111830490+Damarists@users.noreply.github.com>
2023-10-30 23:09:13 -0500

    Update ReviewResource.cs

--- backend/CargaSinEstres.API/CargaSinEstres.API/CargaSinEstres/Resources/ReviewResource.cs
+++ backend/CargaSinEstres.API/CargaSinEstres.API/CargaSinEstres/Resources/ReviewResource.cs
@@ -1,8 +1,10 @@
-using Swashbuckle.AspNetCore.Annotations;
-
 namespace CargaSinEstres.API.CargaSinEstres.Resources;
 
 public class ReviewResource
 {
+    public int Id { get; set; }
 
+    public int Rating { get; set; }
+
+    public string Comment { get; set; }
 }
