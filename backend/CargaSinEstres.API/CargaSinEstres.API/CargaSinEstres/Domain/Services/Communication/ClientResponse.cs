f9331309cbca227b3b9f7684f25fa93bbd8fce1a
Damarists <111830490+Damarists@users.noreply.github.com>
2023-10-30 23:04:35 -0500

    Add files via upload

--- backend/CargaSinEstres.API/CargaSinEstres.API/CargaSinEstres/Domain/Services/Communication/ClientResponse.cs
+++ backend/CargaSinEstres.API/CargaSinEstres.API/CargaSinEstres/Domain/Services/Communication/ClientResponse.cs
@@ -1,8 +1,15 @@
 using CargaSinEstres.API.CargaSinEstres.Domain.Models;
-using CargaSinEstres.API.Shared.Domain.Services.Communication;
+using CargaSinEstres.API.CargaSinEstres.Shared.Domain.Services.Communication;
 
 namespace CargaSinEstres.API.CargaSinEstres.Domain.Services.Communication;
 
-public class ClientResponse : BaseResponse<Client>{
+public class ClientResponse : BaseResponse<Client>
+{
+    public ClientResponse(string message) : base(message)
+    {
+    }
 
+    public ClientResponse(Client resource) : base(resource)
+    {
+    }
 }
