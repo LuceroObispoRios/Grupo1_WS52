715dea5fa2e1ed46d697852d21f036f6251d4164
Damarists <111830490+Damarists@users.noreply.github.com>
2023-10-30 23:07:38 -0500

    Add files via upload

--- backend/CargaSinEstres.API/CargaSinEstres.API/CargaSinEstres/Persistence/Repositories/ReviewRepository.cs
+++ backend/CargaSinEstres.API/CargaSinEstres.API/CargaSinEstres/Persistence/Repositories/ReviewRepository.cs
@@ -1,11 +1,23 @@
 using CargaSinEstres.API.CargaSinEstres.Domain.Models;
 using CargaSinEstres.API.CargaSinEstres.Domain.Repositories;
-using CargaSinEstres.API.Shared.Persistence.Contexts;
+using CargaSinEstres.API.CargaSinEstres.Shared.Persistence.Contexts;
+using CargaSinEstres.API.CargaSinEstres.Shared.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace CargaSinEstres.API.Shared.Persistence.Repositories;
 
-public class ReviewRepository : BaseRepository, IReviewRepository{
+public class ReviewRepository : BaseRepository, IReviewRepository {
+    public ReviewRepository(AppDbContext context) : base(context)
+    {
+    }
 
-    
+    public async Task<IEnumerable<Review>> ListAsync()
+    {
+        return await _context.Reviews.ToListAsync();
+    }
+
+    public async Task AddAsync(Review review)
+    {
+        await _context.Reviews.AddAsync(review);
+    }
 }
