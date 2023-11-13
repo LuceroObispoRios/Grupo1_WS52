127946dc374da1c9d0997b57ac76c0cdde0617a0
Andrea O'Higgins <u20221b178@upc.edu.pe>
2023-11-13 16:02:13 -0500

    Update IBookingHistoryRepository.cs

--- backend/CargaSinEstres.API/CargaSinEstres/Domain/Repositories/IBookingHistoryRepository.cs
+++ backend/CargaSinEstres.API/CargaSinEstres/Domain/Repositories/IBookingHistoryRepository.cs
@@ -5,6 +5,8 @@ namespace CargaSinEstres.API.CargaSinEstres.Domain.Repositories;
 public interface IBookingHistoryRepository {
     Task<BookingHistory> GetAsync(int id);
     Task<IEnumerable<BookingHistory>> ListAsync();
+    Task<IEnumerable<BookingHistory>> ListByClientIdAsync(int clientId);
+    Task<IEnumerable<BookingHistory>> ListByCompanyIdAsync(int clientId);
     Task AddAsync(BookingHistory booking);
     Task UpdateAsync(BookingHistory booking);
     Task RemoveAsync(BookingHistory booking);
