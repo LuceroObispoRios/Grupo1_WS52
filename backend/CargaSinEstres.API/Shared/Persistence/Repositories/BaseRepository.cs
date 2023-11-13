ac61b81639dffee30ed5b1d72aed00961f0f0eb0
Eric Cuevas <99770864+Ericcuevas03@users.noreply.github.com>
2023-11-13 16:04:45 -0500

    Add files via upload

---
+++ backend/CargaSinEstres.API/Shared/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+using CargaSinEstres.API.Shared.Persistence.Contexts;
+
+namespace CargaSinEstres.API.Shared.Persistence.Repositories;
+
+public class BaseRepository
+{
+    protected readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+}
