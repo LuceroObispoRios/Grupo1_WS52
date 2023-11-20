776d1a8c96d07bf00e2ebedcc88bfe349cee1b75
Andrea O'Higgins <u20221b178@upc.edu.pe>
2023-11-20 01:02:33 -0500

    Update http-common.js

--- carga-sin-estres/src/shared/services/http-common.js
+++ carga-sin-estres/src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
-    headers: { 'Content-type': 'application/json' }
+    baseURL: 'http://cargasinestres-001-site1.ftempurl.com/api/v1',
+    headers: {'Content-type': 'application/json' }
 });
