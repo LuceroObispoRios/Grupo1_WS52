49959df7a5a318511de4f054498f67d94cdc2b97
Andrea O'Higgins <u20221b178@upc.edu.pe>
2023-11-20 00:50:44 -0500

    Update bookingList.component.vue

--- carga-sin-estres/src/bookingHistory/components/bookingList.component.vue
+++ carga-sin-estres/src/bookingHistory/components/bookingList.component.vue
@@ -26,20 +26,30 @@ export default {
     console.log('User type:', this.userType);
 
     this.getAllBookingHistory(this.userId);
+    console.log('Booking history: ', this.bookingList);
   },
   methods: {
     getAllBookingHistory(id){
       if(this.userType === 'client'){
         this.bookingApiService.getBookingsByClient(id)
             .then((response) => {
-              this.bookingList = response.data
-              console.log('data client: ', this.bookingList);
+              this.bookingList = response.data;
+              console.log('get response: ', response.data);
+              console.log('total bookings client: ', this.bookingList);
               this.bookingList.forEach((booking, index) => {
                 booking.counter = index + 1;
+                this.bookingApiService.getById(booking.idCompany)
+                    .then((response) => {
+                      booking.hiredCompanyName = response.data.name;
+                      booking.hiredCompanyLogo = response.data.photo;
+                    })
+                    .catch((error) => {
+                      this.errors.push(error);
+                    });
               });
               this.bookingList.reverse();
               this.bookingList.forEach((booking, index) => {
-                booking.chat = booking.chat.reverse();
+                booking.chats = booking.chats.reverse();
               });
               console.log('data client reverse with counter: ', this.bookingList);
             })
@@ -50,13 +60,22 @@ export default {
         this.bookingApiService.getBookingsByCompany(id)
             .then((response) => {
               this.bookingList = response.data;
+              console.log('get response: ', response.data);
               console.log('data company: ', this.bookingList);
               this.bookingList.forEach((booking, index) => {
                 booking.counter = index + 1;
+                this.bookingApiService.getById(booking.idCompany)
+                    .then((response) => {
+                      booking.hiredCompanyName = response.data.name;
+                      booking.hiredCompanyLogo = response.data.photo;
+                    })
+                    .catch((error) => {
+                      this.errors.push(error);
+                    });
               });
               this.bookingList.reverse();
               this.bookingList.forEach((booking, index) => {
-                booking.chat = booking.chat.reverse();
+                booking.chats = booking.chats.reverse();
               });
               console.log('data company reverse with counter: ', this.bookingList);
             })
